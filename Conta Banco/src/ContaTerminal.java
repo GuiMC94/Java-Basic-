ea8fd26cb22223d19a17f2e1b5122cba21369582
Guimc <guilmc@outlook.com>
2024-05-12 19:03:36 -0300

    nao consigo imprimir variavel tipo double

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -1,9 +1,42 @@
-public class ContaTerminal {
-
-     public static void main(String[] args) throws Exception{
-        System.out.println("Hello World");
-         
-     }
 
 
-}
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {   
+    public static void main(String[] args) throws Exception {
+
+      Scanner scr  = new Scanner(System.in).useLocale(Locale.US);
+ 
+        System.out.println("Digite o Numero da sua Conta");
+        int numero = scr.nextInt();
+
+        System.out.println("Digite a Agencia");
+        String agencia = scr.next();
+
+        System.out.println("Digite seu Nome Completo");
+        String nomeCompleto  = scr.next();
+
+        System.out.println("Valor Saldo");
+        double saldo = scr.nextDouble();
+
+       
+
+        System.out.println("Meu numero da conta" + numero);
+        System.out.println(agencia);
+        System.out.println(nomeCompleto);
+        System.out.println( " Meu valor é " + saldo + "real" );
+
+    }
+  }
+
+
+        
+
+
+
+
+
+
+
+        
