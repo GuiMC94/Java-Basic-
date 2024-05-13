612178e217db29bd5a50ab2c41d8b7c09c00fd3b
Guimc <guilmc@outlook.com>
2024-05-13 00:08:35 -0300

    Concluido Desafio Criar Conta Bancaria

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -1,34 +1,50 @@
-
-
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ContaTerminal {   
-    public static void main(String[] args) throws Exception {
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite sua agencia");
+        String agencia = scanner.next();
+        
+        System.out.println("Digite seu nome Completo");
+        String nomeCliente = scanner.next();
 
-      Scanner scr  = new Scanner(System.in).useLocale(Locale.US);
- 
-        System.out.println("Digite o Numero da sua Conta");
-        int numero = scr.nextInt();
+        System.out.println("Digite o numero da conta");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
 
-        System.out.println("Digite a Agencia");
-        String agencia = scr.next();
-
-        System.out.println("Digite seu Nome Completo");
-        String nomeCompleto  = scr.next();
-
-        System.out.println("Valor Saldo");
-        double saldo = scr.nextDouble();
-
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println(agencia);
+        System.out.println(nomeCliente);
+        System.out.println(numero);
+        System.out.println("Meu saldo é " + saldo + "reais ");
+        
+        
+    }
+}
        
 
-        System.out.println("Meu numero da conta" + numero);
-        System.out.println(agencia);
-        System.out.println(nomeCompleto);
-        System.out.println( " Meu valor é " + saldo + "real" );
 
-    }
-  }
+        
+       
+        
+        
+    
+
+
+
+
+        
+
+        
+
+
 
 
         
